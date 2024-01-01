75e96056c83b16d034cd2ed08c64c3dcd4734966
Deepak Kapoor <deepakkapoor16@gmail.com>
2024-01-01 15:17:53 +0530

    for loops folder

---
+++ for loops/forLoops.js
@@ -0,0 +1,27 @@
+//  Example 1: Print the first 10 natural numbers using for loop.
+let n = 11;
+for (let i=1; i<n; i++) {
+    console.log(i);
+}
+
+// program to print all the even numbers within the given range.
+for (let i=1; i<n; i++) {
+    if (i%2==0){
+    console.log(i);
+}
+}
+
+// program to calculate the sum of all numbers from 1 to a given number
+let sum = 0;
+for (let i = 0; i <= n; i++) {
+    sum += i;
+}
+console.log(sum);
+
+
+
+
+
+
+
+
---
+++ for loops/forLoops.py
@@ -0,0 +1,15 @@
+# Example 1: Print the first 10 natural numbers using for loop.
+n = 11
+for i in range(n):
+    print(i)
+    
+# program to print all the even numbers within the given range.
+for i in range(n):
+    if i%2==0:
+        print(i)
+        
+# program to calculate the sum of all numbers from 1 to a given number
+sum = 0
+for i in range(n + 1):
+    sum += i
+print(sum)
